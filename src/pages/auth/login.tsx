a34a435750913c625632fab67765c9337f7bfe5f
ismael <ismontelongo@outlook.com>
2026-05-05 17:39:17 +0100

    Update: si estas logeado y entras  login/register de devuelve al dashboard

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,17 +1,28 @@
 import { LoginForm } from "@/components/login-form"
+import { useAuth } from '@/hooks/auth/UseAuth'
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
-  return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            {/* icono aqui */}
-          </div>
-          Acme Inc.
-        </a>
-        <LoginForm />
+  const context = useAuth();
+  const navigate = useNavigate();
+
+  if (context.user) {
+    return(
+      navigate("/dashboard")
+    );
+  } else {
+    return (
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a href="#" className="flex items-center gap-2 self-center font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              {/* icono aqui */}
+            </div>
+            Acme Inc.
+          </a>
+          <LoginForm />
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,17 +1,28 @@
 import { SignupForm } from "@/components/signup-form"
+import { useAuth } from '@/hooks/auth/UseAuth'
+import { useNavigate } from 'react-router-dom';
 
 export default function SignupPage() {
-  return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            {/* icono aqui */}
-          </div>
-          Acme Inc.
-        </a>
-        <SignupForm />
+  const context = useAuth();
+  const navigate = useNavigate();
+
+  if (context.user) {
+    return(
+      navigate("/dashboard")
+    );
+  } else {
+    return (
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a href="#" className="flex items-center gap-2 self-center font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              {/* icono aqui */}
+            </div>
+            Acme Inc.
+          </a>
+          <SignupForm />
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
